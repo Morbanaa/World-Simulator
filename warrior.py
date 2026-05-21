1eb3670ff6bdda374360b3e0cbd8a2dba65e2ada
Morbanaa <theodorerodd@gmail.com>
2026-05-21 18:18:53 -0500

    Set up constructor with position of self and home as well as stats

--- warrior.py
+++ warrior.py
@@ -1,1 +1,8 @@
-ECHO is on.
+class Warrior():
+    def __init__(self,ypos,xpos,homeY,homeX,clan,attack):
+        self.ypos = ypos
+        self.xpos = xpos
+        self.homeY = homeY
+        self.homeX = homeX
+        self.clan = clan
+        self.attack = attack
