975911ef6a17b962f8ef01eab405845e6d5b03ab
Morbanaa <theodorerodd@gmail.com>
2026-05-21 18:18:08 -0500

    Constructor sets up location of self and home as well as other data

--- villager.py
+++ villager.py
@@ -1,1 +1,8 @@
-ECHO is on.
+class Villager():
+    def __init__(self,ypos,xpos,homeY,homeX,clan):
+        self.ypos = ypos
+        self.xpos = xpos
+        self.homeY = homeY
+        self.homeX = homeX
+        self.clan = clan
+        self.item = "none"
