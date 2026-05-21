803d1870aa82c2687e3d66c852c3a228f5f897a4
Morbanaa <theodorerodd@gmail.com>
2026-05-21 14:27:36 -0500

    Added movement for the cursor, will add selecting later

--- cursor.py
+++ cursor.py
@@ -1,1 +1,23 @@
-ECHO is on.
+# President
+# TR JG
+# Idiotball Studios
+
+import keyboard
+
+class Cursor():
+    def __init__(self,ypos,xpos):
+        self.ypos = ypos
+        self.xpos = xpos
+
+    def update(self,game_map):
+        if keyboard.is_pressed("W") and game_map[self.ypos -1][self.xpos] != "@":
+            self.ypos -= 1
+
+        if keyboard.is_pressed("S") and game_map[self.ypos +1][self.xpos] != "@":
+            self.ypos += 1
+
+        if keyboard.is_pressed("A") and game_map[self.ypos][self.xpos -1] != "@":
+            self.xpos -= 1
+
+        if keyboard.is_pressed("D") and game_map[self.ypos][self.xpos +1] != "@":
+            self.xpos += 1
