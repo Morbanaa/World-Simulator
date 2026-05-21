ddba1254490c83caa11fa866f579f09d9ea09278
Morbanaa <theodorerodd@gmail.com>
2026-05-21 14:23:19 -0500

    Set up main game loop which renders the map each frame and updates the objects

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-ECHO is on.
+# President
+# TR JG
+# Idiotball Studios
+
+import time
+from game_manager import Game_Manager
+
+def main():
+    game_speed = 0.05
+    game_manager = Game_Manager(40,100)
+    game_manager.world_gen()
+    while True:
+
+        game_manager.update()
+        game_manager.render_world()
+        game_manager.clear_move_cursor()
+        time.sleep(game_speed)
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
